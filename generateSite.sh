51e4ba2ce5c0782aa913f8949dbd3b472586c9a8
esa <esa.hekmat@gmail.com>
2015-09-23 15:59:41 +0330

    #MG-6 create build script for creating site from static contents

---
+++ generateSite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -d "jbake-2.4.0" ]; then
+  echo "jbake not installed! try to download it"
+  wget "http://jbake.org/files/jbake-2.4.0-bin.zip"
+  unzip jbake-2.4.0-bin.zip
+fi
+rm -rf src/main/resources/public
+jbake-2.4.0/bin/jbake src/main/resources/website src/main/resources/public
